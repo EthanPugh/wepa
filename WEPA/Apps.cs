7204255bcfbd8b5a62ff5b2dd9e9ea922f594add
Ethan Pugh <ethanthomaspugh@hotmail.com>
2021-05-08 22:55:25 +0100

    Full cleanup and compile

--- WEPA/Apps.cs
+++ WEPA/Apps.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 /*
  * Hardcoded list of preinstalled Windows applications that
@@ -12,44 +11,45 @@ namespace WEPA
     public static class Apps
     {
         // Returns a list of applications in the form of delimiter-separated strings.
-        public static List<String> GetList()
+        public static IEnumerable<string> GetList()
         {
-            List<String> apps = new List<String>();
-
-            apps.Add("3D Viewer`No Data`microsoft3dviewer");
-            apps.Add("Alarms & Clock`Personal Information, Calendar`windowsalarms");
-            apps.Add("Calculator`No Data`windowscalculator");
-            apps.Add("Camera`Personal Information, Location`windowscamera");
-            apps.Add("Cortana`Personal Information, Contacts, Location, Calendar, Relationships`549981C3F5F10");
-            apps.Add("Feedback Hub`Personal Information, Email`windowsfeedbackhub");
-            apps.Add("Films & TV`Personal Information, Usage Data`zunevideo");
-            apps.Add("Get Help`Personal Information, Email`gethelp");
-            apps.Add("Groove Music`Personal Information, Usage Data`zunemusic");
-            apps.Add("Mail and Calendar`Personal Information, Contacts, Calendar, Email, Usage Data`windowscommunicationsapps");
-            apps.Add("Maps`Personal Information, Location, Contacts`windowsmaps");
-            apps.Add("Microsoft Edge`Personal Information`microsoftedge.stable");
-            apps.Add("Mixed Reality Portal`Personal Information`mixedreality.portal");
-            apps.Add("Office Hub`Personal Information`microsoftofficehub");
-            apps.Add("OneNote`Personal Information, Calendar`office.onenote");
-            apps.Add("Paint 3D`No Data`mspaint");
-            apps.Add("People`Personal Information, Calendar, Contacts, Location`people");
-            apps.Add("Photos`Personal Information, Contacts, Location`windows.photos");
-            apps.Add("Skype`Personal Information, Contacts, Email`skypeapp");
-            apps.Add("Snip & Sketch`Personal Information, Usage Data`screensketch");
-            apps.Add("Solitaire Collection`Personal Information, Usage Information`microsoftsolitairecollection");
-            apps.Add("Sticky Notes`No Data`microsoftstickynotes");
-            apps.Add("Store Checkout`Personal Information, Credentials`storepurchaseapp");
-            apps.Add("Tips`Personal Information`getstarted");
-            apps.Add("Voice Recorder`Personal Information`windowssoundrecorder");
-            apps.Add("Wallet`Personal Information, Credentials`wallet");
-            apps.Add("Weather`Location`bingweather");
-            apps.Add("Xbox Console Companion`Personal Information, Accounts, Contacts, Location`xboxapp");
-            apps.Add("Xbox Game Bar Plugin`Personal Information, Accounts, Contacts, Location`xboxgameoverlay");
-            apps.Add("Xbox Game Bar`Personal Information, Accounts, Contacts, Location`xboxgamingoverlay");
-            apps.Add("Xbox Identity Provider`Personal Information, Accounts, Contacts, Location`xboxidentityprovider");
-            apps.Add("Xbox Speech to Text Overlay`Personal Information, Accounts, Contacts, Location`xboxspeechtotextoverlay");
-            apps.Add("Xbox TCUI`Personal Information, Accounts, Contacts, Location`xbox.tcui");
-            apps.Add("Your Phone`Personal Information, Contacts, Phone`yourphone");
+            var apps = new List<string>
+            {
+                "3D Viewer`No Data`microsoft3dviewer",
+                "Alarms & Clock`Personal Information, Calendar`windowsalarms",
+                "Calculator`No Data`windowscalculator",
+                "Camera`Personal Information, Location`windowscamera",
+                "Cortana`Personal Information, Contacts, Location, Calendar, Relationships`549981C3F5F10",
+                "Feedback Hub`Personal Information, Email`windowsfeedbackhub",
+                "Films & TV`Personal Information, Usage Data`zunevideo",
+                "Get Help`Personal Information, Email`gethelp",
+                "Groove Music`Personal Information, Usage Data`zunemusic",
+                "Mail and Calendar`Personal Information, Contacts, Calendar, Email, Usage Data`windowscommunicationsapps",
+                "Maps`Personal Information, Location, Contacts`windowsmaps",
+                "Microsoft Edge`Personal Information`microsoftedge.stable",
+                "Mixed Reality Portal`Personal Information`mixedreality.portal",
+                "Office Hub`Personal Information`microsoftofficehub",
+                "OneNote`Personal Information, Calendar`office.onenote",
+                "Paint 3D`No Data`mspaint",
+                "People`Personal Information, Calendar, Contacts, Location`people",
+                "Photos`Personal Information, Contacts, Location`windows.photos",
+                "Skype`Personal Information, Contacts, Email`skypeapp",
+                "Snip & Sketch`Personal Information, Usage Data`screensketch",
+                "Solitaire Collection`Personal Information, Usage Information`microsoftsolitairecollection",
+                "Sticky Notes`No Data`microsoftstickynotes",
+                "Store Checkout`Personal Information, Credentials`storepurchaseapp",
+                "Tips`Personal Information`getstarted",
+                "Voice Recorder`Personal Information`windowssoundrecorder",
+                "Wallet`Personal Information, Credentials`wallet",
+                "Weather`Location`bingweather",
+                "Xbox Console Companion`Personal Information, Accounts, Contacts, Location`xboxapp",
+                "Xbox Game Bar Plugin`Personal Information, Accounts, Contacts, Location`xboxgameoverlay",
+                "Xbox Game Bar`Personal Information, Accounts, Contacts, Location`xboxgamingoverlay",
+                "Xbox Identity Provider`Personal Information, Accounts, Contacts, Location`xboxidentityprovider",
+                "Xbox Speech to Text Overlay`Personal Information, Accounts, Contacts, Location`xboxspeechtotextoverlay",
+                "Xbox TCUI`Personal Information, Accounts, Contacts, Location`xbox.tcui",
+                "Your Phone`Personal Information, Contacts, Phone`yourphone"
+            };
 
             return apps;
         }
